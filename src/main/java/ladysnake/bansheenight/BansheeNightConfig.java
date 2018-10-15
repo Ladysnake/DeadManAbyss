b9b0d2ea4f7aef2bb492d8cf620ad2b9d6b09d4f
Pyrofab <redstoneinfire@gmail.com>
2018-10-15 22:19:58 +0200

    Fix the config gui not saving changes

--- src/main/java/ladysnake/bansheenight/BansheeNightConfig.java
+++ src/main/java/ladysnake/bansheenight/BansheeNightConfig.java
@@ -1,8 +1,13 @@
 package ladysnake.bansheenight;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = BansheeNight.MOD_ID)
+@Mod.EventBusSubscriber(modid = BansheeNight.MOD_ID)
 public class BansheeNightConfig {
 
     @Config.Name("Minimum Ticks Between Nights")
@@ -35,15 +40,22 @@ public class BansheeNightConfig {
 
         @Config.Name("On Return From Portal")
         @Config.Comment("Trigger a banshee night when returning from another dimension")
-        public boolean portal = true;
+        public boolean portal = false;
 
         //FIXME implement! ^Up
         @Config.Name("On Sunset")
         @Config.Comment("Trigger a banshee night when the sun sets")
-        public boolean sunset = true;
+        public boolean sunset = false;
 
-        @Config.Name("On Lotus Ritual")
-        @Config.Comment({"Trigger a banshee night when tossing a black lotus into water around midnight", "(this does NOT respect the probability, but the minimum tick cooldown)"})
+        @Config.Name("On Blind Quartz Ritual")
+        @Config.Comment({"Trigger a banshee night when tossing a blind quartz into water around midnight", "(this does NOT respect the probability, but the minimum tick cooldown)"})
         public boolean lotusRitual = true;
+    }
+
+
+    @SubscribeEvent
+    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if(event.getModID().equals(BansheeNight.MOD_ID))
+            ConfigManager.sync(BansheeNight.MOD_ID, Config.Type.INSTANCE);
     }
 }
