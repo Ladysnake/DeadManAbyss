807eb6fba44f93f2af9da551c33b84830fe4f8d5
Pyrofab <redstoneinfire@gmail.com>
2018-10-16 11:43:54 +0200

    Add "query" argument to the command

--- src/main/java/ladysnake/deadmanabyss/command/CommandDma.java
+++ src/main/java/ladysnake/deadmanabyss/command/CommandDma.java
@@ -2,14 +2,19 @@ package ladysnake.deadmanabyss.command;
 
 import ladysnake.deadmanabyss.api.event.DmaEventHandler;
 import ladysnake.deadmanabyss.capability.CapabilityDmaEvent;
-import net.minecraft.command.*;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class CommandDma extends CommandBase {
@@ -25,33 +30,42 @@ public class CommandDma extends CommandBase {
 
     @Override
     public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
-        if(args.length == 1) return getListOfStringsMatchingLastWord(args, "on", "off");
-        else if(args.length == 2) return getListOfStringsMatchingLastWord(args, Arrays.stream(server.worlds).filter(w -> w.getCapability(CapabilityDmaEvent.CAPABILITY_DMA_EVENT, null) != null).map(w -> w.provider.getDimension()).collect(Collectors.toList()));
+        if (args.length == 1) return getListOfStringsMatchingLastWord(args, "on", "off");
+        else if (args.length == 2)
+            return getListOfStringsMatchingLastWord(args, Arrays.stream(server.worlds).filter(w -> w.getCapability(CapabilityDmaEvent.CAPABILITY_DMA_EVENT, null) != null).map(w -> w.provider.getDimension()).collect(Collectors.toList()));
         else return Collections.emptyList();
     }
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
         if (args.length > 0) {
-            boolean flag;
-            if (args[0].equals("on")) {
-                flag = true;
-            } else if (args[0].equals("off")) {
-                flag = false;
+            Boolean flag;
+            switch (args[0]) {
+                case "on":
+                    flag = true;
+                    break;
+                case "off":
+                    flag = false;
+                    break;
+                case "query":
+                    // Just show whether the event is occuring
+                    flag = null;
+                    break;
+                default:
+                    throw new CommandException(getUsage(sender));
             }
-            else throw new CommandException(getUsage(sender));
 
             World world = args.length == 2 ? server.getWorld(parseInt(args[1])) : sender.getEntityWorld();
             DmaEventHandler cap = world.getCapability(CapabilityDmaEvent.CAPABILITY_DMA_EVENT, null);
             if (cap != null) {
-                if(flag) cap.startEvent();
-                else cap.stopEvent();
+                if (flag != null) {
+                    if (flag) cap.startEvent();
+                    else cap.stopEvent();
+                }
                 sender.sendMessage(new TextComponentTranslation("dma.command.status." + (cap.isEventOccuring() ? "on" : "off")));
-            }
-            else {
+            } else {
                 throw new CommandException("dma.command.no_cap");
             }
-        }
-        else throw new WrongUsageException(getUsage(sender));
+        } else throw new WrongUsageException(getUsage(sender));
     }
 }
